b98701a005544930cdc85a12ad9c07e34878af7b
sKITAURA <126636224+sKITAURA@users.noreply.github.com>
2023-03-17 04:23:09 +0000

    [finish]2-6

---
+++ lesson6-1.rb
@@ -0,0 +1,7 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+input_number1 = gets.to_i
+input_number2 = gets.to_i
+puts "計算結果を出力します"
+total = input_number1 * input_number2
+puts "#{input_number1}*#{input_number2}=#{total}"
